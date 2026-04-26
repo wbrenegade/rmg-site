3d4cdb8bca611528fa075d794b6cfb3efd4dc690
root <root@srv1559755.hstgr.cloud>
2026-04-26 02:46:53 +0000

    fix web routes

--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -1,18 +1,31 @@
 const express = require("express");
+
 const {
-	serveIndex,
-	servePage,
-	serveToolsIndex,
-	serveToolPage,
-	pageRoutes,
-	toolRoutes
+  serveIndex,
+  servePage,
+  serveToolsIndex,
+  serveToolPage,
+  serveOrderSuccess,
+  serveCheckout,
+  serveCart,
+  pageRoutes,
+  toolRoutes
 } = require("../controllers/pageController");
 
 const router = express.Router();
 
+const pagePattern = pageRoutes.join("|");
+const toolPattern = toolRoutes.join("|");
+
 router.get("/", serveIndex);
+
 router.get("/tools", serveToolsIndex);
-router.get(`/tools/:tool(${toolRoutes.join("|")})`, serveToolPage);
-router.get(`/:page(${pageRoutes.join("|")})`, servePage);
+router.get(`/tools/:tool(${toolPattern})`, serveToolPage);
+
+router.get("/checkout", serveCheckout);
+router.get("/cart", serveCart);
+router.get("/order-success", serveOrderSuccess);
+
+router.get(`/:page(${pagePattern})`, servePage);
 
 module.exports = router;
